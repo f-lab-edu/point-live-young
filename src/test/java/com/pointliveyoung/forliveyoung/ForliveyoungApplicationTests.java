474e3fc4eea7e56f38b16d11ae1227fe067cd653
hyeonjaez <jhjh0022@naver.com>
2025-08-17 01:05:57 +0900

    feat: spring project setting

---
+++ src/test/java/com/pointliveyoung/forliveyoung/ForliveyoungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pointliveyoung.forliveyoung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForliveyoungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
