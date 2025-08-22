ae159f03b701f10197f91d6ec28f0b6f08ad29fc
hyeonjaez <jhjh0022@naver.com>
2025-08-22 14:33:34 +0900

    feat: user entity class

---
+++ src/main/java/com/pointliveyoung/forliveyoung/domain/user/entity/User.java
@@ -0,0 +1,53 @@
+package com.pointliveyoung.forliveyoung.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Setter
+    @Column(nullable = false, length = 50, name = "user_name")
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100, name = "user_email")
+    private String email;
+
+    @Setter
+    @Column(nullable = false, length = 100, name = "user_password")
+    private String password;
+
+    @Setter
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name ="user_role")
+    private UserRole userRole;
+
+    @Column(nullable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    private User(String name, String email, String password, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.userRole = UserRole.USER;
+        this.createdAt = LocalDateTime.now();
+        this.birthDate = birthDate;
+    }
+
+    public static User of(String name, String email, String password, LocalDate birthDate) {
+        return new User(name, email, password, birthDate);
+    }
+}
