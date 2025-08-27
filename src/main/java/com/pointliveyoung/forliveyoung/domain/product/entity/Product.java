1e2d98bde7f2039b0d8be4c4dca6d35658923925
hyeonjaez <jhjh0022@naver.com>
2025-08-27 15:25:48 +0900

    feat: setter 삭제 및 필드값 수정하는 메서드 추가

--- src/main/java/com/pointliveyoung/forliveyoung/domain/product/entity/Product.java
+++ src/main/java/com/pointliveyoung/forliveyoung/domain/product/entity/Product.java
@@ -3,9 +3,10 @@ package com.pointliveyoung.forliveyoung.domain.product.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.Check;
 import jakarta.validation.constraints.Min;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "product")
@@ -17,11 +18,9 @@ public class Product {
     @Column(nullable = false)
     private Integer id;
 
-    @Setter
     @Column(nullable = false, length = 100, name = "product_name")
     private String name;
 
-    @Setter
     @Column(columnDefinition = "TEXT")
     private String description;
 
@@ -35,17 +34,72 @@ public class Product {
     @Check(constraints = "price > 0")
     private Integer price;
 
-    @Setter
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Category category;
 
-    public Product(String name, String description, Integer stock, Integer price, Category category) {
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    private Product(String name, String description, Integer stock, Integer price, Category category) {
         this.name = name;
         this.description = description;
         this.stock = stock;
         this.price = price;
         this.category = category;
+        this.isDeleted = false;
+    }
+
+    public static Product create(String name, String description, Integer stock, Integer price, Category category) {
+        if (Objects.isNull(name) || name.isEmpty()) {
+            throw new IllegalArgumentException("상품명은 비어있을 수 없습니다.");
+        }
+        if (Objects.isNull(stock) || stock < 0) {
+            throw new IllegalArgumentException("재고 수량은 0 이상이어야 합니다.");
+        }
+        if (Objects.isNull(price) || price <= 0) {
+            throw new IllegalArgumentException("가격은 0원 이상이어야 합니다.");
+        }
+        if (Objects.isNull(category)) {
+            throw new IllegalArgumentException("카테고리는 비어있을 수 없습니다.");
+        }
+        return new Product(name, description, stock, price, category);
+    }
+
+    public void changeName(String name) {
+        if (Objects.isNull(name) || name.isEmpty()) {
+            throw new IllegalArgumentException("상품명은 비어있을 수 없습니다.");
+        }
+        this.name = name;
+    }
+
+    public void changeDescription(String description) {
+        this.description = description;
+    }
+
+    public void changeStock(Integer stock) {
+        if (Objects.isNull(stock) || stock < 0) {
+            throw new IllegalArgumentException("재고 수량은 0 이상이어야 합니다.");
+        }
+        this.stock = stock;
+    }
+
+    public void changePrice(Integer price) {
+        if (Objects.isNull(price) || price <= 0) {
+            throw new IllegalArgumentException("가격은 0원 이상이어야 합니다.");
+        }
+        this.price = price;
+    }
+
+    public void changeCategory(Category category) {
+        if (Objects.isNull(category)) {
+            throw new IllegalArgumentException("카테고리는 비어있을 수 없습니다.");
+        }
+        this.category = category;
+    }
+
+    public void delete() {
+        this.isDeleted = true;
     }
 
 }
