0300de52edbe574a96358584516ce688b5e0839d
hyeonjaez <jhjh0022@naver.com>
2025-10-19 03:43:51 +0900

    rename: class 이름 변경 Handler -> Listener

--- src/main/java/com/pointliveyoung/forliveyoung/domain/order/event/PurchaseEmailListener.java
+++ src/main/java/com/pointliveyoung/forliveyoung/domain/order/event/PurchaseEmailListener.java
@@ -8,7 +8,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
-public class PurchaseEmailHandler {
+public class PurchaseEmailListener {
 
     private final EmailService emailService;
 
